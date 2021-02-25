74bc376e4a369db05a16d86b8a343a5edc139390
Gabriel Souza <eugabrielf@gmail.com>
2021-02-25 10:12:09 -0300

    criando layoult

--- src/Component/TelaPrevisaoTempo.js
+++ src/Component/TelaPrevisaoTempo.js
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import "./style.css";
 function TelaPrevisaoTempo() {
-  return <h1>Teste</h1>;
+  const [capitais, setCapitais] = useState({});
+  useEffect(() => {
+    async function request() {
+      const apiPrevisaoTempo = await fetch(
+        "https://apiprevmet3.inmet.gov.br/previsao/capitais"
+      );
+      const response = await apiPrevisaoTempo.json();
+      setCapitais(response);
+    }
+    request();
+  }, []);
+
+  // const { Aracaju } = capitais;
+  // const dia = Aracaju && Aracaju["01/03/2021"];
+  return (
+    <section className="container-principal">
+      <div className="sub-container">
+        <h1>Previsão Do Tempo</h1>
+        <input type="text" placeholder="Insira o nome da cidade" />
+      </div>
+    </section>
+  );
 }
 
 export default TelaPrevisaoTempo;
